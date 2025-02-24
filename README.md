146c6f9b88fc145b45938f1fc04a6b867824a728
KPfromSainP <kyrimer2010@gmail.com>
2025-02-24 12:03:55 +0300

    What works: -web and db were containerized
    What not works:
    -no validation
    -no DTO entities
    -no cool structure

---
+++ README.md
@@ -0,0 +1,13 @@
+# Base User service
+
+## preliminary requirements
+
+- `docker --version` >= 25.0.0 (maybe earlier)
+
+## how to start
+
+- `git clone ...`
+- `cd idfc`
+- `docker compose up`
+- check `localhost:8080` (see the endpoints in doc)
+- 
--- src/main/java/com/kirill/idfc/UserRepository.java
+++ src/main/java/com/kirill/idfc/UserRepository.java
@@ -2,12 +2,10 @@ package com.kirill.idfc;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-//import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
-import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<User, Integer> {
     List<User> findByName(String name);
 }
